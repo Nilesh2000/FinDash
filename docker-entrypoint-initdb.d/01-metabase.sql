0698e646f49afbb038f18cc2809ef296165b3b86
Nilesh D <42713643+Nilesh2000@users.noreply.github.com>
2025-03-25 10:25:24 +0530

    feat(sql): enhance metabase_user privileges by granting ownership and future access to public schema and its tables and sequences

--- docker-entrypoint-initdb.d/01-metabase.sql
+++ docker-entrypoint-initdb.d/01-metabase.sql
@@ -7,11 +7,22 @@ CREATE USER metabase_user WITH PASSWORD 'metabase_password';
 -- Grant database-level privileges (to connect)
 GRANT ALL PRIVILEGES ON DATABASE metabase TO metabase_user;
 
--- Connect to metabase_internal_db to set schema-level privileges
+-- Connect to metabase database
 \connect metabase;
 
 -- Grant schema privileges explicitly
 GRANT ALL ON SCHEMA public TO metabase_user;
+
+-- Make metabase_user the owner of the public schema
+ALTER SCHEMA public OWNER TO metabase_user;
+
+-- Grant privileges on all existing tables
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO metabase_user;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO metabase_user;
+
+-- Grant privileges on future tables and sequences
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO metabase_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO metabase_user;
 
 -- Create read-only user
 CREATE USER metabase_ro WITH PASSWORD 'metabase_ro_password';
