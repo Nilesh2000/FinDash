e3384f31bbd04e5c632fc1de986b9f539ab31cf3
Nilesh D <42713643+Nilesh2000@users.noreply.github.com>
2026-05-05 23:31:07 +0530

    feat(excluded_dates.py): add some more dates

--- nav_loader/src/excluded_dates.py
+++ nav_loader/src/excluded_dates.py
@@ -5,4 +5,9 @@ EXCLUDED_DATES = [
     "2024-06-30",  # Sunday
     "2024-08-31",  # Saturday
     "2024-11-30",  # Saturday
+    "2025-05-31",  # Saturday
+    "2025-08-31",  # Sunday
+    "2025-11-30",  # Sunday
+    "2026-01-31",  # Saturday
+    "2026-02-28",  # Saturday
 ]
